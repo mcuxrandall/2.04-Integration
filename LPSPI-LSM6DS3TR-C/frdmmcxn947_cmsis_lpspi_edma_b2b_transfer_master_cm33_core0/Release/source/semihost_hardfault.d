114c128327db73c2dac1fb5c65d3e740d6c60525
mcuxrandall <randall@wumbology.co>
2026-03-02 20:06:31 -0600

    Delete LPSPI-LSM6DS3TR-C directory
    IMU changed to LSM6DS0

--- LPSPI-LSM6DS3TR-C/frdmmcxn947_cmsis_lpspi_edma_b2b_transfer_master_cm33_core0/Release/source/semihost_hardfault.d
+++
@@ -1,4 +0,0 @@
-source/semihost_hardfault.o source/semihost_hardfault.d: \
- ../source/semihost_hardfault.c \
- /Users/andrew/Documents/MCUXpressoIDE_25.6.136/workspace/frdmmcxn947_cmsis_lpspi_edma_b2b_transfer_master_cm33_core0/source/mcux_config.h
-/Users/andrew/Documents/MCUXpressoIDE_25.6.136/workspace/frdmmcxn947_cmsis_lpspi_edma_b2b_transfer_master_cm33_core0/source/mcux_config.h:
